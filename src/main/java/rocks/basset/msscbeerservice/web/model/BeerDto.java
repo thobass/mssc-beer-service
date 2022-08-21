69a83e89273767450da489f3cf90cd6051f2c9da
BASSET, Thomas <thomas.basset@cgi.com>
2022-08-21 12:01:51 +0200

    Adding beer validation. closes #8

--- src/main/java/rocks/basset/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/rocks/basset/msscbeerservice/web/model/BeerDto.java
@@ -6,6 +6,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -15,13 +19,31 @@ import java.util.UUID;
 @NoArgsConstructor
 @Builder
 public class BeerDto {
+
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
+
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
     private String beerName;
+
+    @NotNull
     private BeerStyleEnum beerStyle;
+
+    @Positive
+    @NotNull
     private Long upc;
+
+    @NotNull
+    @Positive
     private BigDecimal price;
     private Integer quantityOnHand;
 }
