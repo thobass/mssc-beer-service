05ffedfcfc291a91c5da98b39abe509d1bbe32d1
BASSET, Thomas <thomas.basset@cgi.com>
2022-08-22 12:15:39 +0200

    Project Initialisation

--- src/main/java/rocks/basset/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/rocks/basset/msscbeerservice/web/controller/BeerController.java
@@ -1,9 +1,12 @@
 package rocks.basset.msscbeerservice.web.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import rocks.basset.msscbeerservice.repositories.BeerRepository;
+import rocks.basset.msscbeerservice.web.mapper.BeerMapper;
 import rocks.basset.msscbeerservice.web.model.BeerDto;
 
 import javax.validation.Valid;
@@ -11,23 +14,35 @@ import java.util.UUID;
 
 @RequestMapping("/api/v1/beer")
 @RestController
+@RequiredArgsConstructor
 public class BeerController {
+
+    private final BeerMapper beerMapper;
+    private final BeerRepository beerRepository;
 
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
-        //TODO Impl
-        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+
+        return new ResponseEntity<>(beerMapper.beerToBeerDto(beerRepository.findById(beerId).get()), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity saveNewBeer(@Validated @RequestBody BeerDto beerDto){
-        //todo impl
+        beerRepository.save(beerMapper.beerDtoToBeer(beerDto));
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
     public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @Validated @RequestBody BeerDto beerDto){
-        //todo impl
+        beerRepository.findById(beerId).ifPresent(beer -> {
+            beer.setBeerName(beerDto.getBeerName());
+            beer.setBeerStyle(beerDto.getBeerStyle().name());
+            beer.setPrice(beerDto.getPrice());
+            beer.setUpc(beerDto.getUpc());
+
+            beerRepository.save(beer);
+        });
+
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
--- src/test/java/rocks/basset/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/rocks/basset/msscbeerservice/web/controller/BeerControllerTest.java
@@ -5,18 +5,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import rocks.basset.msscbeerservice.domain.Beer;
+import rocks.basset.msscbeerservice.repositories.BeerRepository;
 import rocks.basset.msscbeerservice.web.model.BeerDto;
 import rocks.basset.msscbeerservice.web.model.BeerStyleEnum;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BeerController.class)
+@ComponentScan(basePackages = "rocks.basset.msscbeerservice.web.mapper")
 class BeerControllerTest {
     @Autowired
     MockMvc mockMvc;
@@ -24,16 +32,20 @@ class BeerControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @MockBean
+    BeerRepository beerRepository;
+
     @Test
     void getBeerById() throws Exception {
-        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID()).accept(MediaType.APPLICATION_JSON))
+        given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
+
+        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 
     @Test
     void saveNewBeer() throws Exception {
-
-        BeerDto beerDto = getValidBeerDto();
+        BeerDto beerDto =  getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(post("/api/v1/beer/")
@@ -44,10 +56,10 @@ class BeerControllerTest {
 
     @Test
     void updateBeerById() throws Exception {
-        BeerDto beerDto = getValidBeerDto();
+        BeerDto beerDto =  getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
-        mockMvc.perform(put("/api/v1/beer/"+ UUID.randomUUID())
+        mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID().toString())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(beerDtoJson))
                 .andExpect(status().isNoContent());
