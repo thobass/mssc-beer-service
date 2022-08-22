1f1d41466b9936d18bd8ffe8833fb11475b3a2fc
BASSET, Thomas <thomas.basset@cgi.com>
2022-08-22 13:22:37 +0200

    Documentation generation

--- src/test/java/rocks/basset/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/rocks/basset/msscbeerservice/web/controller/BeerControllerTest.java
@@ -55,7 +55,7 @@ class BeerControllerTest {
                         .param("iscold", "yes")
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andDo(document("v1/beer",
+                .andDo(document("v1/beer-get",
                         pathParameters(
                             parameterWithName("beerId").description("UUID of desired beer to get.")
                         ),
@@ -86,7 +86,7 @@ class BeerControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(beerDtoJson))
                 .andExpect(status().isCreated())
-                .andDo(document("v1/beer",
+                .andDo(document("v1/beer-post",
                         requestFields(
                                 fields.withPath("id").ignored(),
                                 fields.withPath("version").ignored(),
