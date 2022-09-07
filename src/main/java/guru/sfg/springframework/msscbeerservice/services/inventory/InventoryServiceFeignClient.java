0c279ad11caa4a1ff15340399ef02adee4464935
BASSET, Thomas <thomas.basset@cgi.com>
2022-09-07 18:34:08 +0200

    Add Hystrix Circuit Breaker for Inventory Service. Closes #44

--- src/main/java/guru/sfg/springframework/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
+++ src/main/java/guru/sfg/springframework/msscbeerservice/services/inventory/InventoryServiceFeignClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 import java.util.UUID;
 
-@FeignClient(name = "beer-inventory-service")
+@FeignClient(name = "beer-inventory-service", fallback = InventoryServiceFeignClientFailover.class)
 public interface InventoryServiceFeignClient {
 
     @GetMapping(value = BeerInventoryServiceRestTemplateImpl.INVENTORY_PATH)
